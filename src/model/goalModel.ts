9a9e22961fd3c5172d80d9c2e42b5781fe902cd0
Anurag Pramanik <p.anurag@outlook.com>
2022-12-06 21:15:23 +0530

    feat(userModel): add schema for user

--- src/model/goalModel.ts
+++ src/model/goalModel.ts
@@ -2,12 +2,22 @@ import { Schema, model } from 'mongoose'
 
 // Create an interface representing a document in MongoDB
 interface Goal {
+  user: {
+    type: Schema.Types.ObjectId
+    required: boolean
+    ref: string
+  }
   text: string
 }
 
 // Create a schema related to the document interface
 const goalSchema = new Schema<Goal>(
   {
+    user: {
+      type: Schema.Types.ObjectId,
+      required: true,
+      ref: 'User',
+    },
     text: {
       type: String,
       required: [true, 'Please add a text value'],
---
+++ src/model/userModel.ts
@@ -0,0 +1,28 @@
+import { Schema, model, SchemaDefinitionProperty } from 'mongoose'
+
+interface User {
+  name: string
+  email: string
+  password: string
+  timestamps?: SchemaDefinitionProperty<string> | boolean
+}
+
+const userSchema = new Schema<User>({
+  name: {
+    type: String,
+    required: [true, 'Please add your name'],
+  },
+  email: {
+    type: String,
+    required: [true, 'Please add your email id'],
+  },
+  password: {
+    type: String,
+    required: [true, 'Please enter your password'],
+  },
+  timestamps: true,
+})
+
+const User = model<User>('User', userSchema)
+
+export default User
