7f1a16562da3d3548e6a40ca7198eaea1ca02154
Anurag Pramanik <p.anurag@outlook.com>
2022-12-06 13:50:01 +0530

    fix(process.env): fix the process.env error with ts

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,11 +1,8 @@
-// const mongoose = require('mongoose')
-// import { connect } from 'mongoose'
 import mongoose from 'mongoose'
-// import config from '../../config'
 
 const connectDB = async () => {
   try {
-    const connection = await mongoose.connect(process.env.MONGO_URI)
+    const connection = await mongoose.connect(process.env.MONGO_URI!)
 
     console.log(`🟢 Mongo db connected:`, connection.connection.host)
   } catch (error) {
