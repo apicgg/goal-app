857930b8635e7d184fe3ff59941f66caa6870d7c
Anurag Pramanik <p.anurag@outlook.com>
2022-12-17 23:53:47 +0530

    refactor(*): rename dir to server for backend code

---
+++ server/server.ts
@@ -0,0 +1,21 @@
+import express, { Application } from "express";
+import { errorHandler } from "./middleware/errorMiddleware";
+import dotenv from "dotenv";
+import connectDB from "./config/db";
+const port = process.env.PORT || 8000;
+
+dotenv.config();
+
+connectDB();
+
+const app: Application = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.use("/api/goals", require("./routes/goalRoutes"));
+app.use("/api/users", require("./routes/userRoutes"));
+
+app.use(errorHandler);
+
+app.listen(port, () => console.log(`🟢 Server started on port ${port}`));
