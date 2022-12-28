4381d11d90705a4e0ebf5ae2bf16148149a14367
Anurag Pramanik <p.anurag@outlook.com>
2022-12-28 11:48:17 +0530

    feat(login): add login component

--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -1,7 +1,70 @@
-import React from "react";
+import React, { useState } from "react";
+import { FaSignInAlt } from "react-icons/fa";
+
+interface FormData {
+  email: string;
+  password: string;
+}
 
 const Login = () => {
-  return <div>Login</div>;
+  const [formData, setFormData] = useState<FormData>({
+    email: "",
+    password: "",
+  });
+
+  const { email, password } = formData;
+
+  const onChange: React.ChangeEventHandler<HTMLInputElement> | undefined = (
+    event
+  ) => {
+    setFormData((prevState) => ({
+      ...prevState,
+      [event.target.id]: event.target.value,
+    }));
+  };
+
+  const onSubmit: React.FormEventHandler<HTMLFormElement> | undefined = (
+    event
+  ) => {
+    event.preventDefault();
+  };
+
+  return (
+    <>
+      <section className="heading">
+        <h1>
+          <FaSignInAlt /> Login
+        </h1>
+        <p>Login to get started</p>
+      </section>
+
+      <section className="form">
+        <form className="form-group" onSubmit={onSubmit}>
+          <input
+            type="text"
+            className="form-control"
+            id="email"
+            value={email}
+            placeholder="Enter your email"
+            onChange={onChange}
+          />
+          <input
+            type="password"
+            className="form-control"
+            id="password"
+            value={password}
+            placeholder="Enter your password"
+            onChange={onChange}
+          />
+          <div className="form-group">
+            <button className="btn btn-block" type="submit">
+              Login
+            </button>
+          </div>
+        </form>
+      </section>
+    </>
+  );
 };
 
 export default Login;
