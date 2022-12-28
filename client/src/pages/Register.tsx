bc03ae9ae97ee641aef99d711dbd7b9f8ab41dd0
Anurag Pramanik <p.anurag@outlook.com>
2022-12-28 10:59:45 +0530

    feat(client): add client side code

--- client/src/pages/Register.tsx
+++ client/src/pages/Register.tsx
@@ -1,6 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
+
+interface FormData {
+  name: string;
+  email: string;
+  password: string;
+  password2: string;
+}
 
 const Register = () => {
+  const [formData, setFormData] = useState<React.SetStateAction<FormData>>({
+    name: "",
+    email: "",
+    password: "",
+    password2: "",
+  });
+
+  // const { name, email, password, password2 } = formData;
+
   return <div>Register</div>;
 };
 
